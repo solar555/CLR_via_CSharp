e1a8fbbf1656ee7e26643737bda1e1580aec4ae6
AsaWF <shikong.666@163.com>
2018-11-12 17:57:17 +0800

    add Ch19

--- Ch18-1-CustomAttributes/Program.cs
+++ Ch18-1-CustomAttributes/Program.cs
@@ -1,17 +1,198 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Ch18_1_CustomAttributes
 {
-    public class Program
+
+    public sealed class Program
     {
         static void Main(string[] args)
         {
 
+        }
+    }
 
+    /// <summary>
+    /// Ch18_4_CustomAttributes
+    /// </summary>
+    [Serializable]
+    [DefaultMemberAttribute("Go")]
+    [DebuggerDisplayAttribute("Richter", Name = "Jeff", Target = typeof(CustomAttributes))]
+    internal sealed class CustomAttributes
+    {
+        [Conditional("Debug")]
+        [Conditional("Release")]
+        public void DoSomething() { }
+
+        public CustomAttributes() { }
+
+        [STAThread]
+        static void Go(string[] args)
+        {
+            ShowAttributes(typeof(CustomAttributes));
+
+            var members =
+                from m in typeof(CustomAttributes).GetTypeInfo().DeclaredMembers.OfType<MethodBase>()
+                where m.IsPublic
+                select m;
+
+            foreach (MemberInfo member in members)
+            {
+                ShowAttributes(member);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void ShowAttributes(MemberInfo attributeTarget)
+        {
+            var attributes = attributeTarget.GetCustomAttributes<Attribute>();
+
+            Console.WriteLine("Attributes applied to {0}: {1}",
+                attributeTarget.Name, (attributes.Count() == 0 ? "None" : String.Empty));
+
+            foreach (Attribute attribute in attributes)
+            {
+                Console.WriteLine(" {0}", attribute.GetType().ToString());
+
+                if (attribute is DefaultMemberAttribute)
+                    Console.WriteLine(" MemberName={0}",
+                        ((DefaultMemberAttribute)attribute).MemberName);
+                if (attribute is ConditionalAttribute)
+                    Console.WriteLine(" ConditionString={0}",
+                        ((ConditionalAttribute)attribute).ConditionString);
+                if (attribute is CLSCompliantAttribute)
+                    Console.WriteLine(" IsCompliant={0}",
+                        ((CLSCompliantAttribute)attribute).IsCompliant);
+
+                DebuggerDisplayAttribute dda = attribute as DebuggerDisplayAttribute;
+                if (dda != null)
+                    Console.WriteLine(" Value={0}, Name={1}, Target={2}",
+                        dda.Value, dda.Name, dda.Target);
+            }
+            Console.WriteLine();
+        }
+    }
+
+
+    /// <summary>
+    /// Ch18_5
+    /// </summary>
+    internal sealed class MatchingAttributes
+    {
+        [Flags]
+        internal enum Accounts
+        {
+            Savings = 0x0001,
+            Checking = 0x0002,
+            Brokerage = 0x0004
+        }
+
+        [AttributeUsage(AttributeTargets.Class)]
+        internal sealed class AccountsAttribute : Attribute
+        {
+            private Accounts m_accounts;
+
+            public AccountsAttribute(Accounts accounts)
+            {
+                m_accounts = accounts;
+            }
+
+            public override bool Match(object obj)
+            {
+                if (obj == null) return false;
+
+                if (this.GetType() != obj.GetType()) return false;
+
+                AccountsAttribute other = (AccountsAttribute)obj;
+
+                if ((other.m_accounts & m_accounts) != m_accounts)
+                    return false;
+
+                return true;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (obj == null) return false;
+
+                if (this.GetType() != obj.GetType()) return false;
+
+                AccountsAttribute other = (AccountsAttribute)obj;
+
+                if (other.m_accounts != m_accounts)
+                    return false;
+
+                return true;
+            }
+
+            public override int GetHashCode()
+            {
+                return (int)m_accounts;
+            }
+        }
+
+        [Accounts(Accounts.Savings)]
+        internal sealed class ChildAccount { }
+
+        [Accounts(Accounts.Savings | Accounts.Checking | Accounts.Brokerage)]
+        internal sealed class AdultAccount { }
+
+        public sealed class Program
+        {
+            public static void Go()
+            {
+                CanWriteCheck(new ChildAccount());
+                CanWriteCheck(new AdultAccount());
+
+                CanWriteCheck(new Program());
+            }
+
+            private static void CanWriteCheck(object obj)
+            {
+                Attribute checking = new AccountsAttribute(Accounts.Checking);
+                Attribute validAccounts = Attribute.GetCustomAttribute(
+                    obj.GetType(), typeof(AccountsAttribute), false);
+
+                if ((validAccounts != null) && checking.Match(validAccounts))
+                    Console.WriteLine("{0} types can write checks.", obj.GetType());
+                else
+                    Console.WriteLine("{0} types can NOT write checks.", obj.GetType());
+            }
+        }
+    }
+
+    /// <summary>
+    /// Ch18_7
+    /// </summary>
+    /// 
+    internal sealed class ConditionalAttributeExample
+    {
+        [Conditional("TEST")]
+        [Conditional("VERIFY")]
+        internal sealed class CondAttribute : Attribute
+        {
+
+        }
+
+        [Cond]
+        public sealed class Program
+        {
+            public static void Go()
+            {
+                Console.WriteLine("CondAttribute is {0}applied to Program type.",
+                    Attribute.IsDefined(typeof(Program),
+                    typeof(CondAttribute)) ? "" : "not ");
+            }
         }
     }
 }
+
+
---
+++ Ch19-1-NullableValueAttributes/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch19_1_NullableValueAttributes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            NullGetType();
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Ch19.1
+        /// </summary>
+        private static void UsingNullable()
+        {
+            Nullable<int> x = 5;
+            Nullable<int> y = null;
+            Console.WriteLine("x: HasValue={0}, Value={1}", x.HasValue, x.Value);
+            Console.WriteLine("y: HasValue={0}, Value={1}", y.HasValue, y.Value);
+        }
+
+        /// <summary>
+        /// Ch19.1
+        /// </summary>
+        private static void UsingNullablePoint()
+        {
+            Point? p1 = new Point(1, 1);
+            Point? p2 = new Point(2, 2);
+
+            Console.WriteLine("Are points equal? " + (p1 == p2).ToString());
+            Console.WriteLine("Are points not equal? " + (p1 != p2).ToString());
+        }
+
+        /// <summary>
+        /// 19.3.3
+        /// CLR lie
+        /// </summary>
+        private static void NullGetType()
+        {
+            int? x = 5;
+            Console.WriteLine(x.GetType());
+        }
+
+        private static void NullCallInterface()
+        {
+            int? n = 5;
+            int result = ((IComparable)n).CompareTo(5);
+            Console.WriteLine(result);
+        }
+    }
+
+    /// <summary>
+    /// Ch19.1
+    /// </summary>
+    internal struct Point
+    {
+        private int m_x, m_y;
+        public Point(int x, int y)
+        {
+            m_x = x;
+            m_y = y;
+        }
+
+        public static bool operator==(Point p1, Point p2)
+        {
+            return (p1.m_x == p2.m_x) && (p1.m_y == p2.m_y);
+        }
+
+        public static bool operator!=(Point p1, Point p2)
+        {
+            return !(p1 == p2);
+        }
+    }
+}
