4fc0c2da250a316cf5e6e211ae90963d286ea7b0
AsaWF <shikong.666@163.com>
2018-11-13 10:09:08 +0800

    add Ch20

---
+++ Ch20-1-ExceptionHandling/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch20_1_ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    internal static class Mechanics
+    {
+        public static void SomeMethod()
+        {
+            try
+            {
+
+            }
+            catch(InvalidOperationException)
+            {
+
+            }
+            catch (IOException)
+            {
+
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
