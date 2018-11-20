4bc76860ca01eac801656f7eddcd60faae66e36f
AsaWF <shikong.666@163.com>
2018-11-20 11:50:45 +0800

    add Ch24

--- Ch23-1-AssemblyLoadingAndReflection/Program.cs
+++ Ch23-1-AssemblyLoadingAndReflection/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.CSharp.RuntimeBinder;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
@@ -18,6 +19,7 @@ namespace Ch23_1_AssemblyLoadingAndReflection
     {
         static void Main(string[] args)
         {
+            ConvertMethodInfoToRuntimeMethodHandle.Go();
 
             Console.ReadKey();
         }
@@ -269,12 +271,33 @@ namespace Ch23_1_AssemblyLoadingAndReflection
         }
     }
 
+    /// <summary>
+    /// 23.5.2
+    /// </summary>
+    internal static class ReflectionExtensions
+    {
+        // Helper extension method to simplify syntax to create a delegate
+        public static TDelegate CreateDelegate<TDelegate>(
+            this MethodInfo mi,
+            object target = null)
+        {
+            return (TDelegate)(object)mi.CreateDelegate(typeof(TDelegate), target);
+        }
+    }
+
+    /// <summary>
+    /// 23.5.2
+    /// </summary>
     internal static class Invoker
     {
         private sealed class SomeType
         {
             private int m_someField;
-            public SomeType(ref int x) { x *= 2; }
+            public SomeType(ref int x)
+            {
+                x *= 2;
+            }
+
             public override string ToString()
             {
                 return m_someField.ToString();
@@ -297,7 +320,13 @@ namespace Ch23_1_AssemblyLoadingAndReflection
 
         public static void Go()
         {
-
+            Type t = typeof(SomeType);
+            BindToMemberThenInvokeTheMember(t);
+            Console.WriteLine();
+            BindToMemberCreateDelegateToMemberThenInvokeTheMember(t);
+            Console.WriteLine();
+            UseDynamicToBindAndInvokeTheMember(t);
+            Console.WriteLine();
         }
 
         private static void BindToMemberThenInvokeTheMember(Type t)
@@ -326,7 +355,7 @@ namespace Ch23_1_AssemblyLoadingAndReflection
             {
                 pi.SetValue(obj, 0, null);
             }
-            catch(TargetInvocationException e)
+            catch (TargetInvocationException e)
             {
                 if (e.InnerException.GetType() != typeof(ArgumentOutOfRangeException)) throw;
                 Console.WriteLine("Property set catch.");
@@ -340,9 +369,146 @@ namespace Ch23_1_AssemblyLoadingAndReflection
             ei.RemoveEventHandler(obj, eh);
         }
 
+        private static void BindToMemberCreateDelegateToMemberThenInvokeTheMember(Type t)
+        {
+            Console.WriteLine("BindToMemberCreateDelegateToMemberThenInvokeTheMember");
 
+            object[] args = new object[] { 12 };
+            Console.WriteLine("x before constructor called: " + args[0]);
+            object obj = Activator.CreateInstance(t, args);
+            Console.WriteLine("Type: " + obj.GetType().ToString());
+            Console.WriteLine("x after constructor returns: " + args[0]);
+
+            // NOTE: You can't create a delegate to a field
+
+            // Call a method
+            MethodInfo mi = obj.GetType().GetTypeInfo().GetDeclaredMethod("ToString");
+            var toString = mi.CreateDelegate<Func<String>>(obj);
+            string s = toString();
+            Console.WriteLine("ToString: " + s);
+
+            // Read and write a property
+            PropertyInfo pi = obj.GetType().GetTypeInfo().GetDeclaredProperty("SomeProp");
+            var setSomeProp = pi.SetMethod.CreateDelegate<Action<int>>(obj);
+            try
+            {
+                setSomeProp(0);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Property set catch.");
+            }
+            setSomeProp(2);
+            var getSomeProp = pi.GetMethod.CreateDelegate<Func<int>>(obj);
+            Console.WriteLine("SomeProp: " + getSomeProp());
+
+            // Add and remove a delegate from the event
+            EventInfo ei = obj.GetType().GetTypeInfo().GetDeclaredEvent("SomeEvent");
+            var addSomeEvent = ei.AddMethod.CreateDelegate<Action<EventHandler>>(obj);
+            addSomeEvent(EventCallback);
+            var removeSomeEvent = ei.RemoveMethod.CreateDelegate<Action<EventHandler>>(obj);
+            removeSomeEvent(EventCallback);
+        }
+
+        private static void UseDynamicToBindAndInvokeTheMember(Type t)
+        {
+            Console.WriteLine("UseDynamicToBindAndInvokeTheMember");
+
+            // Construct an instance (You can't use dynamic to call a constructor)
+            object[] args = new object[] { 12 };
+            Console.WriteLine("x before constructor called: " + args[0]);
+            dynamic obj = Activator.CreateInstance(t, args);
+            Console.WriteLine("Type: " + obj.GetType().ToString());
+            Console.WriteLine("x after constructor returns: " + args[0]);
+
+            // Read and write to a field
+            try
+            {
+                obj.m_someField = 5;
+                int v = (int)obj.m_someField;
+                Console.WriteLine("someField: " + v);
+            }
+            catch (RuntimeBinderException e)
+            {
+                // We get here because the field is private
+                Console.WriteLine("Faild to access field: " + e.Message);
+            }
+
+            // Call a method
+            string s = (string)obj.ToString();
+            Console.WriteLine("ToString: " + s);
+
+            // Read and write a property
+            try
+            {
+                obj.SomeProp = 0;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Property set catch.");
+            }
+            obj.SomeProp = 2;
+            int val = (int)obj.SomeProp;
+            Console.WriteLine("SomeProp: " + val);
+
+            // Add and remove a delegate from the event
+            obj.SomeEvent += new EventHandler(EventCallback);
+            obj.SomeEvent -= new EventHandler(EventCallback);
+        }
 
         // Callback method added to the event
         private static void EventCallback(Object sender, EventArgs e) { }
+    }
+
+    internal sealed class ConvertMethodInfoToRuntimeMethodHandle
+    {
+        private const BindingFlags c_bf = 
+            BindingFlags.FlattenHierarchy |
+            BindingFlags.Instance | 
+            BindingFlags.Static |
+            BindingFlags.Public | 
+            BindingFlags.NonPublic;
+
+        public static void Go()
+        {
+            Show("Before doing anything");
+
+            List<MethodBase> methodInfos = new List<MethodBase>();
+            foreach (Type t in typeof(object).Assembly.GetExportedTypes())
+            {
+                if (t.IsGenericTypeDefinition) continue;
+
+                MethodBase[] mb = t.GetMethods(c_bf);
+                methodInfos.AddRange(mb);
+            }
+
+            Console.WriteLine("# of methods={0:N0}", methodInfos.Count);
+            Show("After building cache of MethodInfo objects");
+
+            List<RuntimeMethodHandle> methodHandles =
+                methodInfos.ConvertAll<RuntimeMethodHandle>(mb => mb.MethodHandle);
+
+            Show("Holding MethodInfo and RuntimeMethodHandle cache");
+            GC.KeepAlive(methodInfos);
+
+            methodInfos = null;
+            Show("After freeing MethodInfo objects");
+
+            methodInfos = methodHandles.ConvertAll<MethodBase>(
+                rmh => MethodBase.GetMethodFromHandle(rmh));
+            Show("Size of heap after re-creating MethodInfo objects");
+            GC.KeepAlive(methodHandles);
+            GC.KeepAlive(methodInfos);
+
+            methodHandles = null;
+            methodInfos = null;
+            Show("After freeing MethodInfos and RuntimeMethodHandles");
+        }
+
+        private static void Show(string s)
+        {
+            Console.WriteLine("Heap size={0,12:N0} - {1}", 
+                GC.GetTotalMemory(true), s);
+        }
     }
 }
---
+++ Ch24-1-RuntimeSerialization/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class RuntieSerialization
+{
+    static void Main(string[] args)
+    {
+        QuickStart.Go();
+
+        Console.ReadLine();
+    }
+}
+
+internal static class QuickStart
+{
+    public static void Go()
+    {
+        var objectGraph = new List<string> { "Jeff", "Kristin", "Aidan", "Grant" };
+        Stream stream = SerializeToMemory(objectGraph);
+
+        stream.Position = 0;
+        objectGraph = null;
+
+        objectGraph = (List<string>)DeserializeFromMemory(stream);
+        foreach (var s in objectGraph) Console.WriteLine(s);
+
+        var clone = DeepClone(objectGraph);
+        MultipleGraphs();
+        OptInSerialization();
+    }
+
+    private static MemoryStream SerializeToMemory(object objectGraph)
+    {
+        MemoryStream stream = new MemoryStream();
+        BinaryFormatter formatter = new BinaryFormatter();
+        formatter.Serialize(stream, objectGraph);
+        return stream;
+    }
+
+    private static object DeserializeFromMemory(Stream stream)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        return formatter.Deserialize(stream);
+    }
+
+    private static Object DeepClone(object original)
+    {
+        // Construct a temporary memory stream
+        using (MemoryStream stream = new MemoryStream())
+        {
+            // Construct a serialization formatter that daes all the hard work
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            // This line is explained in this chapter's "Streaming Contexts" section
+            formatter.Context = new StreamingContext(StreamingContextStates.Clone);
+
+            // Serialize the object graph into the memory stream
+            formatter.Serialize(stream, original);
+
+            // Seek back to the start of the memory stream before deserializing
+            stream.Position = 0;
+
+            // Deserialize the graph into a new set of objects and
+            // return the root of the graph (deep copy) to the caller
+            return formatter.Deserialize(stream);
+        }
+    }
+
+    [Serializable]
+    private sealed class Customer { /* ... */}
+    [Serializable]
+    private sealed class Order { /* ... */}
+
+    private static List<Customer> s_customers = new List<Customer>();
+    private static List<Order> s_pendingOrders = new List<Order>();
+    private static List<Order> s_processedOrders = new List<Order>();
+
+    private static void MultipleGraphs()
+    {
+        using (var stream = new MemoryStream())
+        {
+            SaveApplicationState(stream);
+            stream.Position = 0;
+            RestoreApplicationState(stream);
+        }
+    }
+
+    private static void SaveApplicationState(Stream stream)
+    {
+        // Construct a serialization formatter that does all the hard work
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        // Serialize our application's entire state
+        formatter.Serialize(stream, s_customers);
+        formatter.Serialize(stream, s_pendingOrders);
+        formatter.Serialize(stream, s_processedOrders);
+    }
+
+    private static void RestoreApplicationState(Stream stream)
+    {
+        // Construct a serialization formatter that does all the hard work
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        // Deserialize our application's entire state (same order as serialized)
+        s_customers = (List<Customer>)formatter.Deserialize(stream);
+        s_pendingOrders = (List<Order>)formatter.Deserialize(stream);
+        s_processedOrders = (List<Order>)formatter.Deserialize(stream);
+    }
+
+    // Not marked [Serializable]
+    private struct Point { public int x, y; }
+
+    private static void OptInSerialization()
+    {
+        Point pt = new Point { x = 1, y = 2 };
+        using (var stream = new MemoryStream())
+        {
+            new BinaryFormatter().Serialize(stream, pt);// throws SerializationException
+        }
+    }
+}
+
